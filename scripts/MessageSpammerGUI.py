255f722221fdf288dc900fc494ff5f8a54d958a6
brainage04 <thomasmcmah21@gmail.com>
2023-11-28 13:24:27 +1000

    made a start on GUI class

---
+++ scripts/MessageSpammerGUI.py
@@ -0,0 +1,9 @@
+class MessageSpammerGUI:
+    def __init__ (self):
+        """Constructor"""
+        self.phrase = "test"
+        self.interval = 0.1
+        self.isAutomatic = True
+        self.maxIterations = 20
+        self.addMessageSuffix = True
+        self.useRandomSuffix = True
